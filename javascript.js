6e6626b2364018dfe9afc2a08f9f9a4280127814
Andrew Siems <siemsdrew@gmail.com>
2022-07-22 10:27:07 -0700

    Add event listener to each square in the grid that changes the background color on mouseover

--- javascript.js
+++ javascript.js
@@ -2,6 +2,15 @@ let gridTotal = 256;
 
 for (i = 0; i < gridTotal; i++) {
     const newDiv = document.createElement("div");
-    newDiv.className = "square";
+    newDiv.className = "squares";
     container.appendChild(newDiv);
 }
+
+const squares = document.querySelectorAll('.squares');
+
+squares.forEach(square => {
+    square.addEventListener('mouseover', function changeColor () {
+        square.setAttribute('style', 'background-color: black;');
+    });
+});
+
