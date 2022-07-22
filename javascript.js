1532eb182325fcb0c8fe1cd216cdf381a12bf257
Andrew Siems <siemsdrew@gmail.com>
2022-07-22 16:10:34 -0700

    Add event listener to button and set up function to generate the new grid size.

--- javascript.js
+++ javascript.js
@@ -1,16 +1,33 @@
-let gridTotal = 256;
 
-for (i = 0; i < gridTotal; i++) {
-    const newDiv = document.createElement("div");
-    newDiv.className = "squares";
-    container.appendChild(newDiv);
-}
 
-const squares = document.querySelectorAll('.squares');
+var gridWidth = '';
+var gridTotal = 0;
 
-squares.forEach(square => {
-    square.addEventListener('mouseover', function changeColor () {
-        square.setAttribute('style', 'background-color: black;');
+const numSquares = document.getElementById('numSquares');
+numSquares.addEventListener('click', function gridSize () {
+    gridWidth = prompt("Please enter the number of squares: ");
+    gridTotal = Number(gridWidth) * Number(gridWidth);
+    //console.log(gridTotal);
+
+    for (i = 0; i < gridTotal; i++) {
+        const newDiv = document.createElement("div");
+        newDiv.className = "squares";
+        container.appendChild(newDiv);
+    }
+
+    //return nodelist containing references to all squares in the grid
+    const squares = document.querySelectorAll('.squares');
+
+    //for each square in our nodelist, add event listener
+    //mouseover is the type of event, and changeColor is the function we want to call on event
+    squares.forEach(square => {
+        square.addEventListener('mouseover', function changeColor () {
+            square.setAttribute('style', 'background-color: black;');
+        });
     });
 });
+
+
+
+
 
