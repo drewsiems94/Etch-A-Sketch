c3c79c269ae7ca281219e268d0f76631afcf0599
Andrew Siems <siemsdrew@gmail.com>
2022-07-25 09:09:20 -0700

    Set initial grid size to 16x16.

--- javascript.js
+++ javascript.js
@@ -2,9 +2,26 @@
 
 var gridWidth = '';
 var gridTotal = 0;
-var colors = ['red', 'blue', 'green', 'yellow', 'orange', 'purple'];
+var colors = ['brown', 'aquamarine', 'red', 'blue', 'green', 'yellow', 'orange', 'purple', 'pink'];
 
+for (i = 0; i < 256; i++) {
+    const newDiv = document.createElement("div");
+    newDiv.className = "squares";
+    container.appendChild(newDiv);
+}
 
+container.style.cssText = `grid-template-columns: repeat(16, 31.25px)`;
+
+//return nodelist containing references to all squares in the grid
+const squares = document.querySelectorAll('.squares');
+
+//for each square in our nodelist, add event listener
+//mouseover is the type of event, and changeColor is the function we want to call on event
+squares.forEach(square => {
+    square.addEventListener('mouseover', function changeColor () {
+        square.setAttribute('style', 'background-color: black;');
+    });
+});
 
 const numSquares = document.getElementById('numSquares');
 numSquares.addEventListener('click', makeGrid);
@@ -64,11 +81,8 @@ function makeColorGrid() {
 
     container.style.cssText = `grid-template-columns: repeat(${gridWidth}, ${500 / gridWidth}px)`;
     
-    //return nodelist containing references to all squares in the grid
     const squares = document.querySelectorAll('.squares');
 
-    //for each square in our nodelist, add event listener
-    //mouseover is the type of event, and changeColor is the function we want to call on event
     squares.forEach(square => {
         square.addEventListener('mouseover', function changeColor () {
             square.style.backgroundColor = randomColor();
